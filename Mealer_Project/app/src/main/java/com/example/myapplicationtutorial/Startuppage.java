e1ad9e9ce4d8019df82a13654738d087e0d70dde
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-09-29 01:18:52 -0400

    FirstProjectCommit_SplashScreen_Nofunctionality
    Create the xml file of the main screen,welcoming a user back screen, creating an account screen.

---
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Startuppage.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Startuppage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startuppage);
+    }
+}
