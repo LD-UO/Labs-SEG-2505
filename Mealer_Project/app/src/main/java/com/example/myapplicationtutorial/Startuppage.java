22537ed98d26caf24151bc6c05d35187bffc4574
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-10-09 01:25:51 -0400

    SplashScreenSetup

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Startuppage.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Startuppage.java
@@ -10,5 +10,6 @@ public class Startuppage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startuppage);
+        getSupportActionBar().hide();
     }
 }
