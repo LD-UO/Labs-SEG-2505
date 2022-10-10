562cf995fcb75ee388a36951c470a9213b8402bf
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-10-09 22:25:38 -0400

    Addednewchefpage

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Startuppage.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Startuppage.java
@@ -2,7 +2,10 @@ package com.example.myapplicationtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Startuppage extends AppCompatActivity {
 
@@ -11,5 +14,25 @@ public class Startuppage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startuppage);
         getSupportActionBar().hide();
+
+        Button create_btn = (Button)findViewById(R.id.createaccbtn2);
+        create_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Startuppage.this,Newchef.class));
+
+
+            }
+
+
+        });
+
     }
-}
+
+
+
+
+
+    }
+
+
