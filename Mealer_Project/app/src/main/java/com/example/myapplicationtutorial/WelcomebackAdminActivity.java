ba6528c68aa3657a4b5c37662c18e44abc9cc024
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-10-17 03:06:57 -0400

    updated user interface

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/WelcomebackAdminActivity.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/WelcomebackAdminActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Accountcreate extends AppCompatActivity {
+public class WelcomebackAdminActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accountcreate);
+        setContentView(R.layout.welcomeback_admin);
     }
 }
