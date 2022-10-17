942c14c99ac3379a2dc1d9c6d95c8943c46b5467
Luke Dusautoy <ldusa012@uottawa.ca>
2022-10-17 14:59:56 -0400

    new client.uploadData() implimented

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Newclient.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Newclient.java
@@ -7,10 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NewClient extends AppCompatActivity {
 
+    private DatabaseReference databaseReference;
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //creating database reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("Client");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newclient);
 
@@ -22,7 +31,15 @@ public class NewClient extends AppCompatActivity {
                 Intent intent = new Intent(NewClient.this, Welcomeback.class);
                 intent.putExtra("userType", "Client");
                 startActivity(intent);
+                uploadData();
             }
         });
+    }
+    protected void uploadData(){
+        //Assumes chef attributes are correct
+
+        Client newClient = new Client(databaseReference.push().getKey(),username,password);
+
+        databaseReference.child(newClient.getId()).setValue(newClient);
     }
 }
