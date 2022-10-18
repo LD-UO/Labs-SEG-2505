bb72a2369d0b56d8395a366d1d7a1f9b054cccd6
Nikolah2418 <ndanc009@uottawa.ca>
2022-10-18 17:55:29 -0400

    Added text validation for newClient and newChef

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/NewChef.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/NewChef.java
@@ -2,10 +2,11 @@ package com.example.myapplicationtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.*;
 
@@ -31,10 +32,22 @@ public class NewChef extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(NewChef.this, Welcomeback.class);
-                intent.putExtra("userType", "Chef");
-                startActivity(intent);
-                uploadData();
+                EditText fullnamenewchef = (EditText) findViewById(R.id.fullnamenewclient);
+                EditText usernamenewchef = (EditText) findViewById(R.id.usernamenewclient);
+                EditText passwordnewchef = (EditText) findViewById(R.id.passwordnewclient);
+                EditText confirmpasswordnewchef = (EditText) findViewById(R.id.confirmpasswordnewclient);
+                if (fullnamenewchef.getText().toString().trim().isEmpty() || usernamenewchef.getText().toString().trim().isEmpty() ||
+                        passwordnewchef.getText().toString().trim().isEmpty() || confirmpasswordnewchef.getText().toString().trim().isEmpty()) {
+                    Log.d("TAG","Username or Password cannot be empty");
+                    //Toast.makeText(GeneralLogin.this, "Username or Password cannot be empty", Toast.LENGTH_LONG).show();
+                } else{
+                    // "The code below will need to be implemented correctly"
+                    //Intent intent = new Intent(GeneralLogin.this, WelcomebackAdminActivity.class);
+                    // Will need to change the text here to reflect database access!
+                    //intent.putExtra("userType", "Undecided");
+                    //startActivity(intent);
+                }
+
             }
         });
     }
--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/NewClient.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/NewClient.java
@@ -2,10 +2,11 @@ package com.example.myapplicationtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,10 +30,22 @@ public class NewClient extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(NewClient.this, Welcomeback.class);
-                intent.putExtra("userType", "Client");
-                startActivity(intent);
-                uploadData();
+                EditText fullnamenewclient = (EditText) findViewById(R.id.fullnamenewclient);
+                EditText usernamenewclient = (EditText) findViewById(R.id.usernamenewclient);
+                EditText passwordnewclient = (EditText) findViewById(R.id.passwordnewclient);
+                EditText confirmpasswordnewclient = (EditText) findViewById(R.id.confirmpasswordnewclient);
+                if (fullnamenewclient.getText().toString().trim().isEmpty() || usernamenewclient.getText().toString().trim().isEmpty() ||
+                        passwordnewclient.getText().toString().trim().isEmpty() || confirmpasswordnewclient.getText().toString().trim().isEmpty()) {
+                    Log.d("TAG","Username or Password cannot be empty");
+                    //Toast.makeText(GeneralLogin.this, "Username or Password cannot be empty", Toast.LENGTH_LONG).show();
+                } else{
+                    // "The code below will need to be implemented correctly"
+                    //Intent intent = new Intent(GeneralLogin.this, WelcomebackAdminActivity.class);
+                    // Will need to change the text here to reflect database access!
+                    //intent.putExtra("userType", "Undecided");
+                    //startActivity(intent);
+                }
+
             }
         });
     }
