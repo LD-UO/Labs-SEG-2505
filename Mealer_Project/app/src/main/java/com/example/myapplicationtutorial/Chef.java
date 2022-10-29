dceb99483e18e31b46c4bd46b15428bdabca7f86
Luke Dusautoy <ldusa012@uottawa.ca>
2022-10-29 18:40:55 -0400

    added suspended attribute to chef

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Chef.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Chef.java
@@ -1,8 +1,21 @@
 package com.example.myapplicationtutorial;
 
 public class Chef extends User{
+
+    private Boolean suspended;
+
     public Chef(String id, String username, String password,String fullname) {
         super(id,username,password,fullname);
+
+        suspended = false; //starts as non suspended
+    }
+
+    public Boolean getSuspended(){
+        return suspended;
+    }
+
+    public void setSuspended(Boolean x){
+        suspended = x;
     }
 }
 
