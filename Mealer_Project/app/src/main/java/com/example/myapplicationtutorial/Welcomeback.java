a3af296c7b64073d7c6365165ec5c3dcb8d56d7e
Nikolah2418 <ndanc009@uottawa.ca>
2022-11-03 14:29:32 -0400

    Added login page for clint/chef and a logoff option to go back to the login screen

--- Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Welcomeback.java
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/Welcomeback.java
@@ -1,23 +1,10 @@
 package com.example.myapplicationtutorial;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class Welcomeback extends AppCompatActivity {
 
@@ -29,9 +16,9 @@ public class Welcomeback extends AppCompatActivity {
         setContentView(R.layout.activity_welcomeback2);
         Intent intent = getIntent();
         String userType = intent.getStringExtra("usertype");
-        //TextView welcomeBackMessage = (TextView) findViewById(R.id.textView2);
-        //TextView subheading = (TextView) findViewById(R.id.textView8);
-        //subheading.setText("Welcome! You are signed in as a " + userType);
+        //TextView welcomeBackMessage = (TextView) findViewById(R.id.signedin_chefclient);
+        TextView subheading = (TextView) findViewById(R.id.signedin_chefclient);
+        subheading.setText("Welcome! You are signed in as a " + userType);
 
     }
 
