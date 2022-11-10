b23437c770862318c2a94de1ff3d49f52ea7271e
Luke Dusautoy <ldusa012@uottawa.ca>
2022-11-10 14:53:20 -0500

    Create MenuList.java

---
+++ Mealer_Project/app/src/main/java/com/example/myapplicationtutorial/MenuList.java
@@ -0,0 +1,40 @@
+package com.example.myapplicationtutorial;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MenuList {
+
+        private Activity context;
+        List<Complaint> complaints;
+
+        public MenuList(Activity context, List<Complaint> complaints){
+            super(context, R.layout.menu_list, complaints);
+            this.context = context;
+            this.complaints = complaints;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = context.getLayoutInflater();
+            View listViewItem = inflater.inflate(R.layout.complaint_list, null, true);
+
+            TextView chefUsername = (TextView) listViewItem.findViewById(R.id.chefUsername);
+            TextView description = (TextView) listViewItem.findViewById(R.id.description);
+            Complaint complaint = complaints.get(position);
+            chefUsername.setText(complaint.getChefUsername());
+            description.setText(complaint.getDescription());
+
+            return listViewItem;
+        }
+
+
+
+    }
+
+}
